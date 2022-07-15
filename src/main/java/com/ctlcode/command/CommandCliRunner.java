632f7704deb3f227f1e66e24ea087726a4ea273a
yimao <alfred22529@gmail.com>
2022-07-15 11:11:42 +0800

    add info

--- src/main/java/com/ctlcode/command/CommandCliRunner.java
+++ src/main/java/com/ctlcode/command/CommandCliRunner.java
@@ -3,6 +3,8 @@ package com.ctlcode.command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
 import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
@@ -12,13 +14,22 @@ import java.util.concurrent.Callable;
 public class CommandCliRunner implements Callable<Integer> {
     private static final Logger logger = LoggerFactory.getLogger(CommandCliRunner.class);
 
-    @CommandLine.Option(names = {"-action", "-a"}, required = true)
+    @CommandLine.Option(names = {"-action"}, required = true)
     private String action;
 
     @Override
     public Integer call() throws Exception {
         try {
             System.out.println("call action: " + action);
+
+            if ("info".equals(action)) {
+                SystemInfo systemInfo = new SystemInfo();
+                HardwareAbstractionLayer hal = systemInfo.getHardware();
+                String hardwareUUID = hal.getComputerSystem().getHardwareUUID();
+                String serialNumber = hal.getComputerSystem().getSerialNumber();
+                System.out.printf("hardwareUUID: %s, serialNumber: %s", hardwareUUID, serialNumber);
+            }
+
             return 0;
         } catch (Exception e) {
             System.err.println(e.getMessage());
